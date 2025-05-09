20ce4ba7b0413a168ac274abbe6247db570ed866
Aleksa <bonixsrbija@gmail.com>
2025-05-09 09:45:59 +0200

    New Weapon

---
+++ Content/Items/Weapons/Magic/AshenNatureScepter.cs
@@ -0,0 +1,54 @@
+﻿
+
+
+
+using NeoParacosm.Content.Projectiles.Friendly.Magic;
+using Terraria.DataStructures;
+
+namespace NeoParacosm.Content.Items.Weapons.Magic;
+
+public class AshenNatureScepter : ModItem
+{
+    public override void SetStaticDefaults()
+    {
+        Item.staff[Type] = true;
+    }
+
+    public override void SetDefaults()
+    {
+        Item.damage = 18;
+        Item.DamageType = DamageClass.Magic;
+        Item.width = 48;
+        Item.height = 48;
+        Item.useTime = 45;
+        Item.useAnimation = 45;
+        Item.useStyle = ItemUseStyleID.Shoot;
+        Item.knockBack = 3;
+        Item.value = Item.buyPrice(gold: 3);
+        Item.rare = ItemRarityID.Orange;
+        Item.UseSound = SoundID.Item8;
+        Item.autoReuse = true;
+        Item.mana = 12;
+        Item.shoot = ModContent.ProjectileType<AshenBolt>();
+        Item.shootSpeed = 10;
+        Item.noMelee = true;
+    }
+
+    public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+    {
+        if (Main.myPlayer == player.whoAmI)
+        {
+            Projectile.NewProjectile(source, position, velocity, type, damage, knockback, player.whoAmI, 120, ai2: 1);
+        }
+        return false;
+    }
+
+    public override void AddRecipes()
+    {
+        Recipe recipe = CreateRecipe();
+        recipe.AddIngredient(ModContent.ItemType<NatureScepter>());
+        recipe.AddIngredient(ItemID.HellstoneBar, 10);
+        recipe.AddTile(TileID.Hellforge);
+        recipe.Register();
+    }
+}
---
+++ Content/Projectiles/Friendly/Magic/AshenBolt.cs
@@ -0,0 +1,85 @@
+﻿
+
+using NeoParacosm.Common.Utils;
+
+namespace NeoParacosm.Content.Projectiles.Friendly.Magic;
+
+public class AshenBolt : ModProjectile
+{
+    ref float AITimer => ref Projectile.ai[0];
+    ref float speed => ref Projectile.ai[1];
+    ref float doBurst => ref Projectile.ai[2];
+    NPC closestNPC;
+
+    public override void SetStaticDefaults()
+    {
+        ProjectileID.Sets.TrailCacheLength[Projectile.type] = 8;
+        ProjectileID.Sets.TrailingMode[Projectile.type] = 0;
+    }
+
+    public override void SetDefaults()
+    {
+        Projectile.width = 24;
+        Projectile.height = 24;
+        Projectile.friendly = true;
+        Projectile.timeLeft = 240;
+        Projectile.penetrate = 2;
+        Projectile.Opacity = 0f;
+        Projectile.usesLocalNPCImmunity = true;
+        Projectile.localNPCHitCooldown = 30;
+    }
+
+    public override void AI()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, DustID.OrangeStainedGlass, Scale: 1.5f).noGravity = true;
+        }
+        if (AITimer <= 0)
+        {
+            if (doBurst == 0)
+            {
+                closestNPC = LemonUtils.GetClosestNPC(Projectile, 800);
+                if (closestNPC != null)
+                {
+                    speed += 0.3f;
+                    Projectile.velocity = (closestNPC.Center - Projectile.Center).SafeNormalize(Vector2.Zero) * speed;
+                }
+            }
+        }
+        else
+        {
+            Projectile.velocity /= 1.05f;
+        }
+
+        if (AITimer == 120)
+        {
+            if (doBurst == 1)
+            {
+                Projectile.penetrate = 1;
+            }
+            else
+            {
+                Projectile.penetrate = 2;
+            }
+        }
+
+        AITimer--;
+    }
+
+    public override void OnKill(int timeLeft)
+    {
+        if (doBurst == 1)
+        {
+            if (Main.netMode != NetmodeID.MultiplayerClient)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    LemonUtils.QuickProj(Projectile, Projectile.Center, Vector2.UnitY.RotatedByRandom(6.28f) * Main.rand.NextFloat(8, 12), Type, ai0: 120, ai2: 0);
+                }
+            }
+        }
+        //SoundEngine.PlaySound(SoundID.Dig, Projectile.position);
+        LemonUtils.DustCircle(Projectile.Center, 8, 10, DustID.TintableDustLighted, 7f, color: Color.Orange);
+    }
+}
