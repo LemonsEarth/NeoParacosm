653091f1274942bd78bbe53d8041b2ee4b6e73f4
Aleksa <bonixsrbija@gmail.com>
2025-12-23 23:29:57 +0100

    Add Pebble Strike spell and Pebble projectile
    Introduces the PebbleStrikeSpell magic item and its associated Pebble projectile, including code, textures, and localization entries. Also adds placeholder textures for SandBurstSpell and SandWallSpell, and updates the StoneHelmet_Head sprite.

---
+++ Content/Items/Weapons/Magic/Spells/Earth/PebbleStrikeSpell.cs
@@ -0,0 +1,44 @@
+﻿using NeoParacosm.Content.Projectiles.Friendly.Magic;
+using NeoParacosm.Core.Systems.Assets;
+using System.Collections.Generic;
+using Terraria.Audio;
+using Terraria.DataStructures;
+
+namespace NeoParacosm.Content.Items.Weapons.Magic.Spells.Earth;
+
+public class PebbleStrikeSpell : BaseSpell
+{
+    public override int AttackCooldown => 10;
+    public override int ManaCost => 12;
+    public override Vector2 TargetVector { get; set; } = Main.MouseWorld;
+
+    public override void SpellAction(Player player)
+    {
+        TargetVector = Main.MouseWorld;
+        if (LemonUtils.NotClient())
+        {
+            SoundEngine.PlaySound(SoundID.Item7, player.Center);
+            for (int i = 0; i < 6; i++)
+            {
+                Vector2 dir = player.DirectionTo(Main.MouseWorld).RotatedBy(Main.rand.NextFloat(-MathHelper.Pi/8, MathHelper.Pi/8));
+                Projectile.NewProjectile(Item.GetSource_FromAI(), player.Center,
+                    dir * Main.rand.NextFloat(14, 16) * player.NPCatalystPlayer().ElementalExpertiseBoosts[SpellElement.Earth],
+                    ProjectileType<Pebble>(),
+                    (int)(GetDamage(player) * 0.5f),
+                    1f,
+                    player.whoAmI);
+            }
+        }
+    }
+
+    public override void SetDefaults()
+    {
+        base.SetDefaults();
+        Item.damage = 6;
+        Item.width = 40;
+        Item.height = 38;
+        Item.value = Item.buyPrice(gold: 1);
+        Item.rare = ItemRarityID.Blue;
+        SpellElements = [SpellElement.Earth];
+    }
+}
---
+++ Content/Projectiles/Friendly/Magic/Pebble.cs
@@ -0,0 +1,78 @@
+﻿
+using Microsoft.Xna.Framework.Graphics;
+using NeoParacosm.Common.Utils.Prim;
+
+namespace NeoParacosm.Content.Projectiles.Friendly.Magic;
+
+public class Pebble : ModProjectile
+{
+    ref float AITimer => ref Projectile.ai[0];
+
+    static BasicEffect BasicEffect;
+
+    public override void Load()
+    {
+        if (Main.dedServ) return;
+        Main.RunOnMainThread(() =>
+        {
+            BasicEffect = new BasicEffect(PrimHelper.GraphicsDevice)
+            {
+                TextureEnabled = true,
+                VertexColorEnabled = true,
+            };
+        });
+    }
+
+    public override void Unload()
+    {
+        if (Main.dedServ) return;
+        Main.RunOnMainThread(() =>
+        {
+            BasicEffect?.Dispose();
+            BasicEffect = null;
+        });
+    }
+
+    public override void SetStaticDefaults()
+    {
+        ProjectileID.Sets.TrailCacheLength[Projectile.type] = 12;
+        ProjectileID.Sets.TrailingMode[Projectile.type] = 2;
+        Main.projFrames[Type] = 3;
+    }
+
+    public override void SetDefaults()
+    {
+        Projectile.width = 14;
+        Projectile.height = 14;
+        Projectile.hostile = false;
+        Projectile.friendly = true;
+        Projectile.ignoreWater = false;
+        Projectile.tileCollide = true;
+        Projectile.penetrate = 2;
+        Projectile.timeLeft = 15;
+        Projectile.usesLocalNPCImmunity = true;
+        Projectile.localNPCHitCooldown = 60;
+    }
+
+    public override void AI()
+    {
+        if (AITimer == 0)
+        {
+            Projectile.frame = Main.rand.Next(0, 3);
+        }
+
+        Projectile.rotation = Projectile.velocity.ToRotation();
+        AITimer++;
+    }
+
+    public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+    {
+       
+    }
+
+    public override bool PreDraw(ref Color lightColor)
+    {
+        //PrimHelper.DrawBasicProjectilePrimTrailTriangular(Projectile, Color.White, Color.Black * 0, BasicEffect, topDistance: 7, bottomDistance: 7);
+        return true;
+    }
+}
