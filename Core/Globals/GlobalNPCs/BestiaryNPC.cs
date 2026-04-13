b25468f3715b89e2f70abd17a47b532838cb4395
LemonEarth <bonixsrbija@gmail.com>
2026-04-13 16:06:48 +0200

    Bestiary entries

---
+++ Core/Globals/GlobalNPCs/BestiaryNPC.cs
@@ -0,0 +1,25 @@
+﻿using NeoParacosm.Content.Items.Placeable.Special.ResearcherQuestTiles;
+using NeoParacosm.Core.Globals.GlobalNPCs.Evil;
+using Terraria.DataStructures;
+using Terraria.GameContent.Bestiary;
+using Terraria.Localization;
+
+namespace NeoParacosm.Core.Globals.GlobalNPCs;
+
+public class BestiaryNPC : GlobalNPC
+{
+    public override bool InstancePerEntity => true;
+
+    public override bool AppliesToEntity(NPC entity, bool lateInstantiation)
+    {
+        return entity.ModNPC.Mod == Mod;
+    }
+
+    public override void SetBestiary(NPC npc, BestiaryDatabase database, BestiaryEntry bestiaryEntry)
+    {
+        if (Language.Exists($"Mods.NeoParacosm.NPCs.{npc.TypeName}.Bestiary"))
+        {
+            bestiaryEntry.Info.Add(new FlavorTextBestiaryInfoElement($"Mods.NeoParacosm.NPCs.{npc.TypeName}.Bestiary"));
+        }
+    }
+}
