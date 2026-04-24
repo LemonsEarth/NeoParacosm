f6befe0de2ded401f489d04e7cabde1e3ad15d3a
AleksaMatic1 <bonixsrbija@gmail.com>
2026-04-24 17:08:59 +0200

    Added Mechanical Arm upgrades

---
+++ Content/Items/Accessories/Combat/Melee/BlazingBuster.cs
@@ -0,0 +1,65 @@
+﻿using Terraria.Localization;
+
+namespace NeoParacosm.Content.Items.Accessories.Combat.Melee;
+
+public class BlazingBuster : ModItem
+{
+    readonly float damageBoost = 15f;
+    readonly float attackSpeedBoost = 22f;
+    public override LocalizedText Tooltip => base.Tooltip.WithFormatArgs(damageBoost, attackSpeedBoost);
+    public override void SetDefaults()
+    {
+        Item.width = 60;
+        Item.height = 52;
+        Item.accessory = true;
+        Item.value = Item.buyPrice(0, 80);
+        Item.rare = ItemRarityID.Yellow;
+    }
+
+    public override void UpdateAccessory(Player player, bool hideVisual)
+    {
+        player.GetDamage(DamageClass.Melee) += damageBoost / 100f;
+        player.GetAttackSpeed(DamageClass.Melee) += attackSpeedBoost / 100f;
+        player.pickSpeed -= 0.15f;
+        player.tileSpeed += 0.15f;
+        player.kbGlove = true;
+        player.autoReuseGlove = true;
+        player.meleeScaleGlove = true;
+        player.GetModPlayer<BlazingBusterPlayer>().Active = true;
+    }
+
+    public override void AddRecipes()
+    {
+        Recipe recipe = CreateRecipe();
+        recipe.AddIngredient(ItemType<MechanicalArm>(), 1);
+        recipe.AddIngredient(ItemID.FireGauntlet, 1);
+        recipe.AddTile(TileID.TinkerersWorkbench);
+        recipe.Register();
+    }
+}
+
+public class BlazingBusterPlayer : ModPlayer
+{
+    public bool Active { get; set; } = false;
+
+    public override void ResetEffects()
+    {
+        Active = false;
+    }
+
+    public override void UpdateBadLifeRegen()
+    {
+        if (Active && !Player.ItemTimeIsZero)
+        {
+            Player.DOTDebuff(5);
+        }
+    }
+
+    public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+    {
+        if (Active)
+        {
+            target.AddBuff(BuffID.OnFire3, 300);
+        }
+    }
+}
---
+++ Content/Items/Accessories/Combat/Ranged/InflamedArm.cs
@@ -0,0 +1,35 @@
+﻿using NeoParacosm.Content.Items.Accessories.Combat.Melee;
+using Terraria.Localization;
+
+namespace NeoParacosm.Content.Items.Accessories.Combat.Ranged;
+
+public class InflamedArm : ModItem
+{
+    readonly float attackSpeedBoost = 25f;
+    public override LocalizedText Tooltip => base.Tooltip.WithFormatArgs(attackSpeedBoost);
+    public override void SetDefaults()
+    {
+        Item.width = 60;
+        Item.height = 52;
+        Item.accessory = true;
+        Item.value = Item.buyPrice(0, 75);
+        Item.rare = ItemRarityID.Yellow;
+    }
+
+    public override void UpdateAccessory(Player player, bool hideVisual)
+    {
+        player.GetAttackSpeed(DamageClass.Ranged) += attackSpeedBoost / 100f;
+        player.GetModPlayer<MoltenClockworkPlayer>().Active = true;
+        player.pickSpeed -= 0.15f;
+        player.tileSpeed += 0.15f;
+    }
+
+    public override void AddRecipes()
+    {
+        Recipe recipe = CreateRecipe();
+        recipe.AddIngredient(ItemType<MechanicalArm>(), 1);
+        recipe.AddIngredient(ItemType<MoltenClockwork>(), 1);
+        recipe.AddTile(TileID.TinkerersWorkbench);
+        recipe.Register();
+    }
+}
