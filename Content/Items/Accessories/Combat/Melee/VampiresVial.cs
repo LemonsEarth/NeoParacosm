2064bad64f3a52c6f3a63640535d9e38a9659a6c
AleksaMatic1 <bonixsrbija@gmail.com>
2026-05-31 13:08:50 +0200

    Accessory and helmet

---
+++ Content/Items/Accessories/Combat/Melee/VampiresVial.cs
@@ -0,0 +1,99 @@
+﻿using Terraria.GameContent.ItemDropRules;
+using Terraria.Localization;
+
+namespace NeoParacosm.Content.Items.Accessories.Combat.Melee;
+
+public class VampiresVial : ModItem
+{
+    public override LocalizedText Tooltip => base.Tooltip.WithFormatArgs();
+    public override void SetDefaults()
+    {
+        Item.width = 44;
+        Item.height = 44;
+        Item.accessory = true;
+        Item.value = Item.sellPrice(0, 2);
+        Item.rare = ItemRarityID.Green;
+    }
+
+    public override void UpdateAccessory(Player player, bool hideVisual)
+    {
+        player.GetModPlayer<VampiresVialPlayer>().Active = true;
+    }
+}
+
+public class VampiresVialPlayer : ModPlayer
+{
+    public bool Active { get; set; } = false;
+    int healWindowTimer = 0;
+    int damageTaken = 0;
+    int amountHealed = 0;
+
+    public override void ResetEffects()
+    {
+        Active = false;
+    }
+
+    public override void UpdateBadLifeRegen()
+    {
+
+    }
+
+
+    void Lifesteal()
+    {
+        int amountToHeal = (int)(damageTaken * 0.5f);
+        int healPerHit = amountToHeal / 5;
+        if (healWindowTimer > 0 && damageTaken > 0 && amountHealed < amountToHeal)
+        {
+            Player.Heal(healPerHit);
+            amountHealed += healPerHit;
+        }
+    }
+
+    public override void OnHitNPCWithItem(Item item, NPC target, NPC.HitInfo hit, int damageDone)
+    {
+        if (!Active) return;
+        Lifesteal();
+    }
+
+
+    public override void OnHitNPCWithProj(Projectile proj, NPC target, NPC.HitInfo hit, int damageDone)
+    {
+        if (!Active || !proj.CountsAsTrueMelee()) return;
+        Lifesteal();
+    }
+
+    public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+    {
+
+    }
+
+    public override void OnHurt(Player.HurtInfo info)
+    {
+        if (!Active) return;
+        healWindowTimer = 180;
+        damageTaken = info.Damage;
+        amountHealed = 0;
+    }
+
+    public override void PostUpdateEquips()
+    {
+        if (healWindowTimer > 0)
+        {
+            healWindowTimer--;
+        }
+    }
+}
+
+public class VampiresVialDropNPC : GlobalNPC
+{
+    public override bool AppliesToEntity(NPC entity, bool lateInstantiation)
+    {
+        return entity.type == NPCID.CaveBat;
+    }
+
+    public override void ModifyNPCLoot(NPC npc, NPCLoot npcLoot)
+    {
+        npcLoot.Add(ItemDropRule.Common(ItemType<VampiresVial>(), 50));
+    }
+}
---
+++ Content/Items/Armor/Generic/FrozenFossil/FrozenSkull.cs
@@ -0,0 +1,77 @@
+﻿using NeoParacosm.Content.Items.Armor.Generic.DeathKnight;
+using NeoParacosm.Content.Items.Materials;
+using NeoParacosm.Content.Projectiles.Friendly.Special;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.Localization;
+
+namespace NeoParacosm.Content.Items.Armor.Generic.FrozenFossil;
+
+[AutoloadEquip(EquipType.Head)]
+public class FrozenSkull : ModItem
+{
+    static readonly float critChanceBoost = 6;
+    static readonly float damageBoost = 2;
+    public override LocalizedText Tooltip => base.Tooltip.WithFormatArgs(damageBoost, critChanceBoost);
+
+    public override void SetStaticDefaults()
+    {
+        //ArmorIDs.Head.Sets.
+    }
+
+    public override void SetDefaults()
+    {
+        Item.width = 32;
+        Item.height = 32;
+        Item.defense = 6;
+        Item.rare = ItemRarityID.Green;
+        Item.value = Item.sellPrice(0, 3, 0, 0);
+    }
+
+    public override void UpdateEquip(Player player)
+    {
+        player.GetDamage(DamageClass.Generic).Flat += damageBoost;
+        player.GetCritChance(DamageClass.Generic) += critChanceBoost;
+        player.GetModPlayer<FrozenSkullPlayer>().Active = true;
+    }
+
+    public override void UpdateInventory(Player player)
+    {
+
+    }
+
+    public override void AddRecipes()
+    {
+        Recipe recipe = CreateRecipe();
+        recipe.AddIngredient(ItemType<FrigidFossil>(), 10);
+        recipe.AddTile(TileID.IceMachine);
+        recipe.Register();
+    }
+}
+
+public class FrozenSkullPlayer : ModPlayer
+{
+    public bool Active { get; set; } = false;
+
+    public override void ResetEffects()
+    {
+        Active = false;
+    }
+
+    public override void OnHurt(Player.HurtInfo info)
+    {
+        if (!Active)
+        {
+            return;
+        }
+        SoundEngine.PlaySound(SoundID.Item28, Player.Center);
+        foreach (var npc in Main.ActiveNPCs)
+        {
+            if (npc.CanBeChasedBy() && npc.DistanceSQ(Player.Center) < 300 * 300)
+            {
+                npc.AddBuff(BuffID.Frostburn, 300);
+            }
+        }
+    }
+}
+
