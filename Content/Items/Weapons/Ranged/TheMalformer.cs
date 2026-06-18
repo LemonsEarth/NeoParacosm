65a4bd69b88dc56c29f8b84cc57a9fabbd6fa8ad
AleksaMatic1 <bonixsrbija@gmail.com>
2026-06-19 01:03:28 +0200

    TheMalformer

---
+++ Content/Items/Weapons/Ranged/TheMalformer.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using NeoParacosm.Content.Items.Materials;
+using NeoParacosm.Content.Items.Weapons.Melee;
+using Terraria.DataStructures;
+namespace NeoParacosm.Content.Items.Weapons.Ranged;
+
+public class TheMalformer : ModItem
+{
+    int timer = 0;
+    public override void SetDefaults()
+    {
+        Item.damage = 80;
+        Item.DamageType = DamageClass.Ranged;
+        Item.width = 48;
+        Item.height = 32;
+        Item.useTime = 30;
+        Item.useAnimation = 30;
+        Item.UseSound = SoundID.Item1;
+        Item.useStyle = ItemUseStyleID.Shoot;
+        Item.knockBack = 2;
+        Item.value = Item.buyPrice(gold: 1);
+        Item.rare = ItemRarityID.Red;
+        Item.autoReuse = true;
+        Item.shoot = ProjectileType<TheMalformerHeldProj>();
+        Item.shootSpeed = 30;
+        Item.noMelee = true;
+        Item.noUseGraphic = true;
+        Item.channel = true;
+        Item.useAmmo = AmmoID.Bullet;
+    }
+
+    public override bool CanUseItem(Player player)
+    {
+        return true;
+    }
+
+    public override bool PreDrawInInventory(SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
+    {
+        LemonUtils.DrawDreadlordWeaponGlowInInventory(Type, position, scale, spriteBatch);
+        return false;
+    }
+
+    public override bool PreDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, ref float rotation, ref float scale, int whoAmI)
+    {
+        LemonUtils.DrawDreadlordWeaponGlowInWorld(Item, rotation, scale, spriteBatch);
+        return false;
+    }
+
+    public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
+    {
+        type = Item.shoot;
+        position += new Vector2(velocity.SafeNormalize(Vector2.Zero).X * 18, -8);
+    }
+
+    public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+    {
+        Projectile.NewProjectile(source, position, Vector2.Zero, type, damage, knockback, player.whoAmI);
+        return false;
+    }
+
+    public override void AddRecipes()
+    {
+        Recipe recipe = CreateRecipe();
+        recipe.AddIngredient(ItemType<SupremeMusket>(), 1);
+        recipe.AddIngredient(ItemType<SupremeUndertaker>(), 1);
+        recipe.AddIngredient(ItemType<NightmareScale>(), 12);
+        recipe.AddIngredient(ItemType<DivineFlesh>(), 12);
+        recipe.AddTile(TileID.MythrilAnvil);
+        recipe.Register();
+    }
+}
---
+++ Content/Items/Weapons/Ranged/TheMalformerHeldProj.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using NeoParacosm.Content.Projectiles.Friendly.Ranged;
+using NeoParacosm.Core.Systems.Assets;
+using Terraria.Audio;
+using Terraria.GameContent;
+using Terraria.Graphics.Shaders;
+
+namespace NeoParacosm.Content.Items.Weapons.Ranged;
+
+public class TheMalformerHeldProj : ModProjectile
+{
+    int AITimer = 0;
+    ref float ShotCount => ref Projectile.ai[0];
+    bool thrown = false;
+
+    public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+    {
+
+    }
+
+    public override void SetStaticDefaults()
+    {
+        ProjectileID.Sets.TrailCacheLength[Projectile.type] = 12;
+        ProjectileID.Sets.TrailingMode[Projectile.type] = 2;
+        Main.projFrames[Type] = 1;
+    }
+
+    public override void SetDefaults()
+    {
+        Projectile.width = 50;
+        Projectile.height = 28;
+        Projectile.hostile = false;
+        Projectile.friendly = true;
+        Projectile.ignoreWater = false;
+        Projectile.tileCollide = false;
+        Projectile.penetrate = -1;
+        Projectile.timeLeft = 180;
+        Projectile.DamageType = DamageClass.Ranged;
+        Projectile.Opacity = 1f;
+        Projectile.usesLocalNPCImmunity = true;
+        Projectile.localNPCHitCooldown = 20;
+    }
+    float attackRate = 6;
+    public override void AI()
+    {
+        attackRate = 6;
+        Player player = Projectile.GetOwner();
+        if (!player.IsAlive())
+        {
+            Projectile.Kill();
+        }
+        Vector2 playerCenter = player.RotatedRelativePoint(player.MountedCenter);
+        if (Projectile.timeLeft < 30) Projectile.Opacity = MathHelper.Lerp(0, 1, Projectile.timeLeft / 30f);
+        if (AITimer == 0)
+        {
+            rot = player.direction == 1 ? normRot : oppRot;
+        }
+
+        if (ShotCount < 6)
+        {
+            player.heldProj = Projectile.whoAmI;
+            player.SetDummyItemTime(2);
+            Projectile.timeLeft = 180;
+            if (AITimer % attackRate == 0)
+            {
+                if (Main.myPlayer == Projectile.owner)
+                {
+                    Vector2 dir = Projectile.Center.DirectionTo(Main.MouseWorld).RotatedBy(MathHelper.ToRadians(Main.rand.NextFloat(-5, 5)));
+                    player.PickAmmo(player.HeldItem, out int proj, out float _, out int damage, out float knockback, out int usedAmmoItemID);
+                    Vector2 pos = Projectile.Center + Projectile.Center.DirectionTo(Main.MouseWorld) * 5;
+                    LemonUtils.QuickProj(Projectile, pos, dir * Main.rand.NextFloat(10, 16), ProjectileID.IchorBullet);
+                }
+                SoundEngine.PlaySound(ParacosmSFX.UndertakerGunshot with { Volume = 0.2f, SoundLimitBehavior = SoundLimitBehavior.ReplaceOldest, MaxInstances = 1 }, Projectile.Center);
+                rot += -player.direction * MathHelper.ToRadians(15);
+                ShotCount++;
+            }
+            SetPositionRotationDirection(player, player.Center.DirectionTo(Main.MouseWorld).ToRotation());
+            //Projectile.Center = player.Center + player.Center.DirectionTo(Main.MouseWorld) * 5;
+        }
+        if (ShotCount >= 6 && !thrown && AITimer % attackRate == 0)
+        {
+            thrown = true;
+            if (Main.myPlayer == Projectile.owner)
+            {
+                Projectile.velocity = Projectile.Center.DirectionTo(Main.MouseWorld) * 25;
+            }
+            Projectile.netUpdate = true;
+        }
+
+        if (thrown)
+        {
+            Projectile.rotation = MathHelper.ToRadians(AITimer * 12);
+            Projectile.velocity.Y += 0.25f;
+            Projectile.damage = Projectile.originalDamage * 3;
+
+            if (AITimer % 30 == 0)
+            {
+                if (Main.myPlayer == Projectile.owner)
+                {
+                    Projectile.velocity = Projectile.Center.DirectionTo(Main.MouseWorld) * 10;
+                    LemonUtils.QuickProj(
+                        Projectile,
+                        Projectile.Center,
+                        Vector2.UnitX.RotatedBy(AITimer) * Main.rand.NextFloat(10, 16),
+                        ProjectileType<CursedTracerBullet>()
+                        );
+                }
+                Projectile.netUpdate = true;
+            }
+        }
+
+        AITimer++;
+    }
+
+    float rot = 0;
+    float normRot = 0;
+    float oppRot = MathHelper.Pi;
+    void SetPositionRotationDirection(Player player, float movedRotation = 0)
+    {
+        float goalRot = player.Center.DirectionTo(Main.MouseWorld).ToRotation();
+        rot = Utils.AngleLerp(rot, goalRot, 1f / 10f);
+        player.SetCompositeArmFront(true, Player.CompositeArmStretchAmount.Full, rot + -MathHelper.PiOver2);
+        float spriteRot = player.direction == 1 ? 0 : MathHelper.Pi;
+        Projectile.rotation = rot + spriteRot;
+        Projectile.spriteDirection = player.direction;
+    }
+
+    public override bool PreDraw(ref Color lightColor)
+    {
+
+        return true;
+
+    }
+
+    public override void PostDraw(Color lightColor)
+    {
+        //Main.spriteBatch.End();
+        //Main.spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, Main.DefaultSamplerState, default, Main.Rasterizer, null, Main.GameViewMatrix.TransformationMatrix);
+    }
+}
