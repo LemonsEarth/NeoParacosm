914ed554bd4c5c9983b8ce2b7280afc7fecd15ef
AleksaMatic1 <bonixsrbija@gmail.com>
2026-06-19 15:58:17 +0200

    arcane crystal + snippets

---
+++ Content/Items/Accessories/Combat/Summon/ArcaneStone.cs
@@ -0,0 +1,72 @@
+﻿using NeoParacosm.Common.RecipeGroups;
+using NeoParacosm.Content.Projectiles.Friendly.Summon;
+using Terraria.GameContent.ItemDropRules;
+using Terraria.Localization;
+
+namespace NeoParacosm.Content.Items.Accessories.Combat.Summon;
+
+public class ArcaneStone : ModItem
+{
+    public override LocalizedText Tooltip => base.Tooltip.WithFormatArgs();
+    public override void SetDefaults()
+    {
+        Item.width = 46;
+        Item.height = 46;
+        Item.accessory = true;
+        Item.value = Item.sellPrice(0, 2);
+        Item.rare = ItemRarityID.Blue;
+    }
+
+    public override void UpdateAccessory(Player player, bool hideVisual)
+    {
+        player.GetModPlayer<ArcaneStonePlayer>().Active = true;
+    }
+}
+
+public class ArcaneStonePlayer : ModPlayer
+{
+    public bool Active { get; set; } = false;
+    public override void ResetEffects()
+    {
+        Active = false;
+    }
+}
+
+public class ArcaneStoneMinionProjectile : GlobalProjectile
+{
+    public override void PostAI(Projectile projectile)
+    {
+        if (projectile.minion && projectile.GetOwner().GetModPlayer<ArcaneStonePlayer>().Active)
+        {
+            if (Main.rand.NextBool(300))
+            {
+                Projectile.NewProjectileDirect(
+                    projectile.GetSource_FromThis(),
+                    projectile.Center,
+                    Vector2.Zero,
+                    ProjectileType<ArcaneStoneProjectile>(),
+                    projectile.damage * 4,
+                    projectile.knockBack,
+                    projectile.owner,
+                    ai0: 1f,
+                    ai1: projectile.width,
+                    ai2: projectile.height
+                    );
+                projectile.Kill();
+            }
+        }
+    }
+}
+
+public class ArcaneStoneDropNPC : GlobalNPC
+{
+    public override bool AppliesToEntity(NPC entity, bool lateInstantiation)
+    {
+        return entity.type == NPCID.Drippler;
+    }
+
+    public override void ModifyNPCLoot(NPC npc, NPCLoot npcLoot)
+    {
+        npcLoot.Add(ItemDropRule.Common(ItemType<ArcaneStone>(), 50, 1, 1));
+    }
+}
---
+++ Content/Projectiles/Friendly/Summon/ArcaneStoneProjectile.cs
@@ -0,0 +1,48 @@
+﻿using NeoParacosm.Content.Dusts;
+using NeoParacosm.Core.Systems.Assets;
+using Terraria.Audio;
+
+namespace NeoParacosm.Content.Projectiles.Friendly.Summon;
+
+public class ArcaneStoneProjectile : ModProjectile
+{
+    public override string Texture => ParacosmTextures.Empty100TexPath;
+
+    int AITimer = 0;
+    ref float ScaleMul => ref Projectile.ai[0];
+    ref float SizeX => ref Projectile.ai[1];
+    ref float SizeY => ref Projectile.ai[2];
+    public override void SetStaticDefaults()
+    {
+        ProjectileID.Sets.MinionShot[Projectile.type] = true;
+    }
+
+    public override void SetDefaults()
+    {
+        Projectile.width = 16;
+        Projectile.height = 16;
+        Projectile.penetrate = 3;
+        Projectile.stopsDealingDamageAfterPenetrateHits = true;
+        Projectile.DamageType = DamageClass.Summon;
+        Projectile.timeLeft = 10;
+        Projectile.tileCollide = false;
+        Projectile.friendly = true;
+    }
+
+    public override void AI()
+    {
+        if (AITimer == 0)
+        {
+            SoundEngine.PlaySound(SoundID.Item17, Projectile.Center);
+            float dustFactor = SizeX / Projectile.width;
+            LemonUtils.DustBurst(8, Projectile.Center, DustType<FireDust>(), 1 * dustFactor, 1 * dustFactor, 0.5f * dustFactor, 1f * dustFactor, Color.Red);
+            Projectile.Resize((int)SizeX * 3, (int)SizeY * 3);
+        }
+        AITimer++;
+    }
+
+    public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+    {
+
+    }
+}
