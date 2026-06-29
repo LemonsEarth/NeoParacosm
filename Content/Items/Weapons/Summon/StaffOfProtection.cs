19891581c5b371b3cdfa19d3e780316d60bbc520
LemonEarth <bonixsrbija@gmail.com>
2026-06-29 15:35:31 +0200

    staff of protection

---
+++ Content/Items/Weapons/Summon/StaffOfProtection.cs
@@ -0,0 +1,47 @@
+﻿using NeoParacosm.Content.Items.Materials;
+using NeoParacosm.Core.Systems.Assets;
+using Terraria.DataStructures;
+
+namespace NeoParacosm.Content.Items.Weapons.Summon;
+
+public class StaffOfProtection : ModItem
+{
+    public override void SetStaticDefaults()
+    {
+        ItemID.Sets.StaffMinionSlotsRequired[Type] = 3;
+    }
+
+    public override void SetDefaults()
+    {
+        Item.width = 68;
+        Item.height = 68;
+        Item.mana = 50;
+        Item.noMelee = true;
+        Item.damage = 2;
+        Item.DamageType = DamageClass.Summon;
+        Item.useStyle = ItemUseStyleID.Swing;
+        Item.useTime = 20;
+        Item.useAnimation = 20;
+        Item.UseSound = SFX.SummonStaff;
+        Item.buffType = BuffType<StaffOfProtectionBuff>();
+        Item.shoot = ProjectileType<StaffOfProtectionMinion>();
+        Item.rare = ItemRarityID.Green;
+        Item.value = Item.sellPrice(0, 3);
+    }
+
+    public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
+    {
+        position = Main.MouseWorld;
+    }
+
+    public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+    {
+        player.AddBuff(Item.buffType, 2);
+        if (Main.myPlayer == player.whoAmI)
+        {
+            var projectile = Projectile.NewProjectileDirect(source, position, velocity, type, damage, knockback, Main.myPlayer);
+            projectile.originalDamage = Item.damage;
+        }
+        return false;
+    }
+}
---
+++ Content/Items/Weapons/Summon/StaffOfProtectionMinion.cs
@@ -0,0 +1,143 @@
+﻿using NeoParacosm.Content.Dusts;
+using NeoParacosm.Content.Projectiles.Friendly.Ranged;
+using System.IO;
+
+namespace NeoParacosm.Content.Items.Weapons.Summon;
+
+public class StaffOfProtectionMinion : ModProjectile
+{
+    ref float AITimer => ref Projectile.ai[0];
+    ref float AttackTimer => ref Projectile.ai[1];
+    ref float Speed => ref Projectile.ai[2];
+
+    public override void SetStaticDefaults()
+    {
+        Main.projFrames[Projectile.type] = 1;
+        ProjectileID.Sets.TrailCacheLength[Projectile.type] = 5;
+        ProjectileID.Sets.TrailingMode[Projectile.type] = 2;
+        ProjectileID.Sets.MinionTargettingFeature[Projectile.type] = true;
+        Main.projPet[Projectile.type] = true;
+        ProjectileID.Sets.MinionSacrificable[Projectile.type] = true;
+        ProjectileID.Sets.SummonTagDamageMultiplier[Type] = 0.25f;
+    }
+
+    public override void SetDefaults()
+    {
+        Projectile.width = 42;
+        Projectile.height = 42;
+        Projectile.tileCollide = false;
+        Projectile.friendly = true;
+        Projectile.hostile = false;
+        Projectile.minion = true;
+        Projectile.DamageType = DamageClass.Summon;
+        Projectile.minionSlots = 3;
+        Projectile.penetrate = -1;
+        Projectile.usesLocalNPCImmunity = true;
+        Projectile.localNPCHitCooldown = 15;
+        Projectile.extraUpdates = 0;
+    }
+
+    public override bool MinionContactDamage()
+    {
+        return false;
+    }
+
+    public override bool? CanCutTiles()
+    {
+        return false;
+    }
+
+    public override void AI()
+    {
+        Player owner = Projectile.GetOwner();
+        if (IsPlayerAlive(owner) == false)
+        {
+            Projectile.Kill();
+            return;
+        }
+
+        if (AITimer == 0)
+        {
+            if (Main.myPlayer == Projectile.owner)
+            {
+                Speed = Main.rand.NextFloat(0.01f, 0.04f);
+            }
+            Projectile.netUpdate = true;
+        }
+        Projectile.rotation = MathHelper.ToRadians(AITimer) - MathHelper.PiOver4;
+        Vector2 pos = owner.Center - Vector2.UnitY.RotatedBy(MathHelper.ToRadians(AITimer)) * 64;
+        Projectile.Center = pos;
+        AITimer++;
+    }
+
+    bool IsPlayerAlive(Player owner)
+    {
+        if (owner.dead || !owner.active)
+        {
+            owner.ClearBuff(BuffType<StaffOfProtectionBuff>());
+            return false;
+        }
+
+        if (owner.HasBuff(BuffType<StaffOfProtectionBuff>()))
+        {
+            Projectile.timeLeft = 2;
+        }
+        return true;
+    }
+
+    public override bool PreDraw(ref Color lightColor)
+    {
+        LemonUtils.DrawGlow(Projectile.Center, Color.White, Projectile.Opacity * 0.5f, Projectile.scale * 2);
+        Projectile.DrawAfterimages(Color.White, 0.5f);
+        Projectile.DrawProjectile(Color.White);
+        return false;
+    }
+
+    public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+    {
+
+    }
+}
+
+public class StaffOfProtectionBuff : ModBuff
+{
+    public override void SetStaticDefaults()
+    {
+        Main.buffNoSave[Type] = true;
+        Main.buffNoTimeDisplay[Type] = true;
+    }
+
+    public override void Update(Player player, ref int buffIndex)
+    {
+        if (player.ownedProjectileCounts[ProjectileType<StaffOfProtectionMinion>()] > 0)
+        {
+            player.buffTime[buffIndex] = 18000;
+        }
+        else
+        {
+            player.DelBuff(buffIndex);
+            buffIndex--;
+        }
+    }
+}
+
+public class StaffOfProtectionBuffPlayer : ModPlayer
+{
+    public override void ModifyHitByProjectile(Projectile proj, ref Player.HurtModifiers modifiers)
+    {
+        if (Player.HasBuff(BuffType<StaffOfProtectionBuff>()))
+        {
+            foreach (var projectile in Main.ActiveProjectiles)
+            {
+                if (projectile.owner == Player.whoAmI && projectile.type == ProjectileType<StaffOfProtectionMinion>())
+                {
+                    if (projectile.Distance(proj.Center) < 60)
+                    {
+                        modifiers.FinalDamage *= 0.9f;
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
