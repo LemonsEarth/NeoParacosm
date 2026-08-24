a3cb8a06f801f245bbcdf7a0bccfe705db49d0e4
LemonEarth <bonixsrbija@gmail.com>
2026-08-24 21:04:42 +0200

    goblin hut

---
+++ Content/Items/Placeable/Machines/GoblinHut.cs
@@ -0,0 +1,114 @@
+﻿using NeoParacosm.Content.NPCs.Friendly.Special;
+using Terraria.DataStructures;
+using Terraria.Enums;
+using Terraria.ObjectData;
+
+namespace NeoParacosm.Content.Items.Placeable.Machines;
+
+// This file contains the tile, tile entity and item
+public class GoblinHut : ModTile
+{
+    public override void SetStaticDefaults()
+    {
+        Main.tileFrameImportant[Type] = true;
+        Main.tileSolid[Type] = false;
+        Main.tileNoAttach[Type] = true;
+        Main.tileLighted[Type] = true;
+
+        MinPick = 0;
+        MineResist = 6f;
+        DustType = DustID.Hay;
+
+        TileID.Sets.DisableSmartCursor[Type] = true;
+
+        TileObjectData.newTile.UsesCustomCanPlace = true;
+        //TileObjectData.newTile.StyleHorizontal = true;
+        //TileObjectData.newTile.StyleWrapLimit = 15;
+        TileObjectData.newTile.Height = 5;
+        TileObjectData.newTile.Width = 4;
+        TileObjectData.newTile.CoordinateWidth = 16;
+        TileObjectData.newTile.CoordinateHeights = [16, 16, 16, 16, 16];
+
+        TileObjectData.newTile.CoordinatePadding = 2;
+        TileObjectData.newTile.Origin = new Point16(0, 0);
+        TileObjectData.newTile.AnchorBottom = new AnchorData(AnchorType.SolidTile | AnchorType.SolidWithTop | AnchorType.Platform, 2, 0);
+        TileObjectData.newTile.HookPostPlaceMyPlayer = GetInstance<GoblinHutTileEntity>().Generic_HookPostPlaceMyPlayer;
+        TileObjectData.addTile(Type);
+
+        AddMapEntry(new Color(110, 80, 0));
+    }
+
+    public override void NumDust(int i, int j, bool fail, ref int num)
+    {
+        num = fail ? 3 : 1;
+    }
+
+    public override void ModifyLight(int i, int j, ref float r, ref float g, ref float b)
+    {
+        r = 0.8f;
+        g = 0.8f;
+        b = 0.8f;
+    }
+
+
+    public override void KillMultiTile(int i, int j, int frameX, int frameY)
+    {
+        GetInstance<GoblinHutTileEntity>().Kill(i, j);
+    }
+}
+
+public class GoblinHutTileEntity : ModTileEntity
+{
+    int timer = 0;
+
+    public override bool IsTileValidForEntity(int x, int y)
+    {
+        Tile tile = Main.tile[x, y];
+        return tile.HasTile && tile.TileType == TileType<GoblinHut>();
+    }
+
+    Point16 CenterPos => Position + new Point16(2, 3);
+
+    public override void Update()
+    {
+        if (timer % 600 == 0)
+        {
+            int thisTECount = 0;
+            foreach (var npc in Main.ActiveNPCs)
+            {
+                if (npc.type == NPCType<FriendlyGoblin>() && npc.ai[1] == ID)
+                {
+                    thisTECount++;
+                }
+            }
+
+            if (thisTECount < 5 && NPC.CountNPCS(NPCType<FriendlyGoblin>()) < 20)
+            {
+                NPC npc = NPC.NewNPCDirect(
+                     new EntitySource_TileEntity(this, "NeoParacosm:GoblinHutSpawn"),
+                    CenterPos.ToWorldCoordinates(),
+                    NPCType<FriendlyGoblin>(),
+                    ai0: 3000, ai1: ID);
+
+            }
+        }
+
+        timer++;
+    }
+}
+
+public class GoblinHutItem : ModItem
+{
+    public override void SetStaticDefaults()
+    {
+        Item.ResearchUnlockCount = 20;
+    }
+
+    public override void SetDefaults()
+    {
+        Item.DefaultToPlaceableTile(TileType<GoblinHut>());
+        Item.width = 64;
+        Item.height = 80;
+        Item.rare = ItemRarityID.Orange;
+    }
+}
---
+++ Content/NPCs/Friendly/Special/FriendlyGoblin.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using NeoParacosm.Content.Dusts;
+using System.IO;
+
+namespace NeoParacosm.Content.NPCs.Friendly.Special;
+
+public class FriendlyGoblin : FriendlyPatrolNPC
+{
+    ref float TileEntityID => ref NPC.ai[1];
+
+    public override void SetDefaults()
+    {
+        base.SetDefaults();
+        NPC.width = 26;
+        NPC.height = 34;
+        NPC.lifeMax = 500;
+        NPC.defense = 3;
+        NPC.damage = 40;
+        NPC.HitSound = SoundID.NPCHit1;
+        NPC.DeathSound = SoundID.NPCDeath1;
+        NPC.value = 0;
+        NPC.knockBackResist = 0.6f;
+        SetMovementValues(
+            movementSpeed: 4f,
+            chaseSpeed: 10f,
+            acceleration: 0.05f,
+            jumpHeight: 7f,
+            fallThroughBottomMargin: 8f);
+        SetPatrolValues(
+            patrolDistance: 200,
+            maxHomePositionDistance: 200,
+            switchHomePositionInterval: 180,
+            switchPatrolPositionInterval: 120);
+        SetAggroValues(
+            aggroRange: 300,
+            maxTargetDistance: 600);
+        SetInvestigatingValues(
+            investigatingDuration: 300
+            );
+        SetAnimationValues(
+            walkingStartFrame: 3,
+            walkingMaxFrame: 4,
+            walkingFrameDuration: 12,
+            jumpingFrame: 1,
+            fallingFrame: 2,
+            idleFrame: 0);
+        SetCombatValues(
+            knockback: 7f,
+            npcHitCooldown: 40,
+            bossHitCooldown: 40);
+    }
+
+    public override void AI()
+    {
+        base.AI();
+    }
+
+    public override void Despawn()
+    {
+        base.Despawn();
+        LemonUtils.DustBurst(10, NPC.Center, DustID.Grass, 2, 2, 0.6f, 1f);
+    }
+
+    public override void HitEffect(NPC.HitInfo hit)
+    {
+        if (NPC.life <= 0)
+        {
+            LemonUtils.DustBurst(10, NPC.Center, DustID.Grass, 2, 2, 0.6f, 1f);
+        }
+        else
+        {
+            Dust.NewDustDirect(NPC.RandomPos(-8, -8), 2, 2, DustID.Grass);
+        }
+    }
+}
