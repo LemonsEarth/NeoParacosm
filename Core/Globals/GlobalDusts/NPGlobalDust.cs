f9ae3aa05b8bf2c1b1cc5ba97210f4d0ea2b955d
LemonEarth <bonixsrbija@gmail.com>
2026-09-23 22:33:14 +0200

    shitpost

---
+++ Core/Globals/GlobalDusts/NPGlobalDust.cs
@@ -0,0 +1,27 @@
+﻿using Terraria.GameContent.Bestiary;
+using Terraria.Localization;
+
+namespace NeoParacosm.Core.Globals.GlobalDusts;
+
+//shitpost
+/*public class NPGlobalDust : ModSystem
+{
+    public override void Load()
+    {
+        On_Main.DrawDust += Main_DrawDust;
+    }
+
+    private static void Main_DrawDust(On_Main.orig_DrawDust orig, Main self)
+    {
+        orig(self);
+        LemonUtils.BeginSpriteBatchProjectile();
+        foreach (var dust in Main.dust)
+        {
+            if (dust.active && DustID.Search.TryGetName(dust.type, out string name))
+            {
+                LemonUtils.DrawText(name, dust.position - Vector2.UnitY * 32 - Main.screenPosition, Color.White, 0, Vector2.Zero, dust.scale * 0.5f);
+            }
+        }
+        Main.spriteBatch.End();
+    }
+}*/
